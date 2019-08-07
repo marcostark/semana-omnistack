d3b87b9e602ce5a3028c8659130909286a417688
Marcos <mrcs.stark@gmail.com>
2019-08-07 19:22:00 -0300

    Listagem dados vindo da api

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 
 import Login from './pages/login/Login';
-import Main from './pages/Main';
+import Main from './pages/main/Main';
 
 export default function Routes(){
     return(
