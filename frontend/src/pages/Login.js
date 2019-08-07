ba35b3605f9e44679f854f5760f5e09943dd7c90
Marcos <mrcs.stark@gmail.com>
2019-08-07 16:52:53 -0300

    Criando componentes

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.svg';
+
+export default function Login() {
+    const [ username, setUsername ] = useState('');
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        console.log(username);
+    }
+
+    return (
+        <div className="login-container">
+            <form onSubmit={handleSubmit}>
+                <img src={logo} alt="TinDev"/>    
+                <input 
+                    placeholder="Digite o seu usuário no GitHub"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <button type="submit">
+                    Entrar
+                </button>
+            </form>    
+
+        </div>   
+    );
+}
