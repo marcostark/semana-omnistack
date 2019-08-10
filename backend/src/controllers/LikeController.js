015089abccc96f138f507646fb9f39eac40defb5
Marcos <mrcs.stark@gmail.com>
2019-08-09 23:58:07 -0300

    Adicionando funcao de dar match

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -2,6 +2,7 @@ const Developer = require('../models/Developer');
 
 module.exports = {
     async store(req, res){
+        console.log(req.io, req.connectedUsers);
 
         const {user } = req.headers // Pegando parametros do header, usuário logado
         const {devId } = req.params // Pegando parametros pela rota, recebendo like
@@ -16,7 +17,18 @@ module.exports = {
 
         // Verifica se o alvo já deu like no usuário logado (match)
         if(targetDev.likes.includes(loggedDev._id)){
-            console.log('DEU MATCH!');
+            //console.log('DEU MATCH!');
+            // Buscar conexao de socket ativa
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if(loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if(targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
